78c89b9a52fe047499bfb68b1ca6370bad5c23d7
lapatric <42653152+lapatric@users.noreply.github.com>
2023-02-10 19:05:59 +0100

    implement chat screen

--- lib/pages/messages_page.dart
+++ lib/pages/messages_page.dart
@@ -2,6 +2,7 @@ import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:jiffy/jiffy.dart';
 import 'package:messenger_app/helpers.dart';
+import 'package:messenger_app/screens/screens.dart';
 import '../models/models.dart';
 import '../theme.dart';
 import '../widgets/widgets.dart';
@@ -46,89 +47,94 @@ class _MessageTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      margin: const EdgeInsets.symmetric(horizontal: 8),
-      decoration: const BoxDecoration(
-          border: Border(
-        bottom: BorderSide(color: Colors.grey, width: 0.2),
-      )),
-      child: Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Avatar.medium(url: messageData.profilePicture),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: Text(messageData.senderName,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(ChatScreen.route(messageData));
+      },
+      child: Container(
+        height: 100,
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        decoration: const BoxDecoration(
+            border: Border(
+          bottom: BorderSide(color: Colors.grey, width: 0.2),
+        )),
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Avatar.medium(url: messageData.profilePicture),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(messageData.senderName,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                              letterSpacing: 0.2,
+                              wordSpacing: 1.5,
+                              fontWeight: FontWeight.w900)),
+                    ),
+                    SizedBox(
+                      height: 20,
+                      child: Text(
+                        messageData.message,
                         overflow: TextOverflow.ellipsis,
                         style: const TextStyle(
-                            letterSpacing: 0.2,
-                            wordSpacing: 1.5,
-                            fontWeight: FontWeight.w900)),
-                  ),
-                  SizedBox(
-                    height: 20,
-                    child: Text(
-                      messageData.message,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                          fontSize: 12, color: AppColors.textFaded),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Text(
-                    messageData.dateMessage.toUpperCase(),
-                    style: const TextStyle(
-                      fontSize: 11,
-                      letterSpacing: -0.2,
-                      fontWeight: FontWeight.w600,
-                      color: AppColors.textFaded,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Container(
-                    width: 18,
-                    height: 18,
-                    decoration: const BoxDecoration(
-                      color: AppColors.secondary,
-                      shape: BoxShape.circle,
-                    ),
-                    child: const Center(
-                      child: Text(
-                        '1',
-                        style: TextStyle(
-                          fontSize: 10,
-                          color: AppColors.textLight,
-                        ),
+                            fontSize: 12, color: AppColors.textFaded),
                       ),
                     ),
-                  )
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      messageData.dateMessage.toUpperCase(),
+                      style: const TextStyle(
+                        fontSize: 11,
+                        letterSpacing: -0.2,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.textFaded,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Container(
+                      width: 18,
+                      height: 18,
+                      decoration: const BoxDecoration(
+                        color: AppColors.secondary,
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Center(
+                        child: Text(
+                          '1',
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: AppColors.textLight,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
